9fcf0342e08d4001cd347936b97ec1ae99563d4b
Yomna Osama <yomna.osamma@gmail.com>
2022-12-10 05:05:20 +0200

    Finishing textures & sampler

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -9,6 +9,17 @@ our::Texture2D* our::texture_utils::empty(GLenum format, glm::ivec2 size){
     our::Texture2D* texture = new our::Texture2D();
     //TODO: (Req 11) Finish this function to create an empty texture with the given size and format
 
+    texture->bind();
+    //Set Unpack Alignment to 4-byte (it means that each row takes multiple of 4 bytes in memory)
+    //Note: this is not necessary since:
+    //- Alignment is 4 by default
+    //- Alignment of 1 or 2 will still work correctly but 8 may cause problems
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+    //Send data to texture
+    //NOTE: the internal format is set to GL_RGBA8 so every pixel contains 4 bytes, one for each channel
+    glTexImage2D(GL_TEXTURE_2D, 0, format, size.x, size.y, 0, format, GL_UNSIGNED_BYTE, nullptr);
+
+    texture->unbind();
     return texture;
 }
 
@@ -35,7 +46,19 @@ our::Texture2D* our::texture_utils::loadImage(const std::string& filename, bool 
     our::Texture2D* texture = new our::Texture2D();
     //Bind the texture such that we upload the image data to its storage
     //TODO: (Req 5) Finish this function to fill the texture with the data found in "pixels"
-    
+    texture->bind();
+    //Set Unpack Alignment to 4-byte (it means that each row takes multiple of 4 bytes in memory)
+    //Note: this is not necessary since:
+    //- Alignment is 4 by default
+    //- Alignment of 1 or 2 will still work correctly but 8 may cause problems
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+    //Send data to texture
+    //NOTE: the internal format is set to GL_RGBA8 so every pixel contains 4 bytes, one for each channel
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    //Generate versions of the texture at smaller level of details (useful for filtering)
+    if(generate_mipmap) glGenerateMipmap(GL_TEXTURE_2D);
+
     stbi_image_free(pixels); //Free image data after uploading to GPU
+    texture->unbind();
     return texture;
 }
--- source/common/texture/texture2d.hpp
+++ source/common/texture/texture2d.hpp
@@ -8,15 +8,18 @@ namespace our {
     class Texture2D {
         // The OpenGL object name of this texture 
         GLuint name = 0;
+
     public:
         // This constructor creates an OpenGL texture and saves its object name in the member variable "name" 
         Texture2D() {
-            //TODO: (Req 5) Complete this function
+            //TODO: (Req 5) Complete this function [DONE]
+            glGenTextures(1, &name);
         };
 
         // This deconstructor deletes the underlying OpenGL texture
         ~Texture2D() { 
-            //TODO: (Req 5) Complete this function
+            //TODO: (Req 5) Complete this function [DONE]
+            glDeleteTextures(1, &name);
         }
 
         // Get the internal OpenGL name of the texture which is useful for use with framebuffers
@@ -26,12 +29,14 @@ namespace our {
 
         // This method binds this texture to GL_TEXTURE_2D
         void bind() const {
-            //TODO: (Req 5) Complete this function
+            //TODO: (Req 5) Complete this function [DONE]
+            glBindTexture(GL_TEXTURE_2D, name);
         }
 
         // This static method ensures that no texture is bound to GL_TEXTURE_2D
         static void unbind(){
-            //TODO: (Req 5) Complete this function
+            //TODO: (Req 5) Complete this function 
+            glBindTexture(GL_TEXTURE_2D, 0);
         }
 
         Texture2D(const Texture2D&) = delete;
